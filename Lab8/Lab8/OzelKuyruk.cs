3b573a0f5971bf50a1edf7055ab7f2101799b6bd
Özgür Can Altınok <48184394+OzgurAltinok@users.noreply.github.com>
2019-04-25 17:05:57 +0300

    Update OzelKuyruk.cs

--- Lab8/Lab8/OzelKuyruk.cs
+++ Lab8/Lab8/OzelKuyruk.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,17 +8,62 @@ namespace Lab8
 {
     class OzelKuyruk
     {
-        ArrayList sayilar;
-        public static int ElemanSayisi;
+        ArrayList sayilar = new ArrayList();
+        private static int elemanSayisi;
+
+        public ArrayList Sayilar { get => sayilar; set => sayilar = value; }
+        public static int ElemanSayisi { get => elemanSayisi; set => elemanSayisi = value; }
+
+        public OzelKuyruk()
+        {
+            
+
+        }
+
+        public void KuyrukIlkHal(int Eleman)
+        {
+            Sayilar.Add(Eleman);
+            ElemanSayisi++;
+        }
 
         public void Enqueue(int YeniEleman)
         {
             //implemente edilecek
+
+            for (int i = 0; i < ElemanSayisi; i++)
+                if ((Int32)Sayilar[i] % 2 == 0)
+                {
+                    Sayilar.Insert(i + 1, YeniEleman);
+                    ElemanSayisi++;
+                    break;
+                }
+
+        }
+
+        public void Goster()
+        {
+            for (int i = 0; i < ElemanSayisi; i++)
+            {
+                Console.WriteLine(Sayilar[i]);
+            }
+             
         }
 
         public void Dequeue()
         {
             //implemente edilecek
+            if(ElemanSayisi!=0)
+            try
+            {
+
+                Sayilar.RemoveAt(ElemanSayisi);
+		ElemanSayisi--;
+            }
+
+            catch(Exception e)
+            {
+                    Console.WriteLine(e);
+            }
 
             //OzelKuyruk boş ise istisna fırlatacak
         }
