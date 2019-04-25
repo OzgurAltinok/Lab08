b3ddfa0f3a7eff7d7efeda71407e10450a05baff
Özgür Can Altınok <48184394+OzgurAltinok@users.noreply.github.com>
2019-04-25 17:06:25 +0300

    Update Program.cs

--- Lab8/Lab8/Program.cs
+++ Lab8/Lab8/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,25 +11,31 @@ namespace Lab8
         static void Main(string[] args)
         {
             OzelKuyruk x = new OzelKuyruk();
-            x.Enqueue(1);
-            x.Enqueue(5);
-            x.Enqueue(7);
-            x.Enqueue(9);
-            x.Enqueue(16);
-            x.Enqueue(24);
-            x.Enqueue(7);
+            x.KuyrukIlkHal(1);
+            x.KuyrukIlkHal(5);
+            x.KuyrukIlkHal(7);
+            x.KuyrukIlkHal(9);
+            x.KuyrukIlkHal(16);
+            x.KuyrukIlkHal(24);
+            x.KuyrukIlkHal(7);
 
-            Queue y = new Queue();
-            y.Enqueue(1);
-            y.Enqueue(5);
-            y.Enqueue(7);
-            y.Enqueue(9);
-            y.Enqueue(16);
-            y.Enqueue(24);
-            y.Enqueue(7);
+            x.Enqueue(6);
+            x.Dequeue();
+            x.Goster();
+                
+            //Queue y = new Queue();
+            //y.Enqueue(1);
+            //y.Enqueue(5);
+            //y.Enqueue(7);
+            //y.Enqueue(9);
+            //y.Enqueue(16);
+            //y.Enqueue(24);
+            //y.Enqueue(7);
 
             //Çeşitli ekleme ve çıkarma yaptıktan sonra kuyrukların görüntüsünü adım adım ekranda gösteriniz
             //yukarıdaki eklemeler
+
+            Console.ReadKey();
         }
     }
 }
